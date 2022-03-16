b64988d403993bb2b38f3bdcf232c5d5cd5c9dbc
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-03-16 12:38:41 +0900

    fixed.

---
+++ scripts/install_for_mac/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install visual-studio-code --cask
+brew install lima docker
+
+# install twingate from appstore
+# install sourcetree from web
+# install slack from web
+# install mysqlworkbench
