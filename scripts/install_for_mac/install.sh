a4f8258765dfbc71d7ff50623f08aca1a85a5f35
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-07-19 14:35:05 +0900

    fixed.

--- scripts/install_for_mac/install.sh
+++ scripts/install_for_mac/install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#--------------------------------------------------------------
 # docker/docker-compse/lima
-# brew install lima docker docker-compose
-# limactl start docker.yaml --tty=false
-# limactl list
-# docker context create lima --docker "host=$(limactl list docker --format 'unix://{{.Dir}}/sock/docker.sock')"
-# docker context use lima
+#--------------------------------------------------------------
+sudo chown -R $(whoami) $(brew --prefix)/*
+brew install lima docker docker-compose
+limactl start docker.yaml --tty=false
+limactl list
+
+docker context create lima --docker "host=$(limactl list docker --format 'unix://{{.Dir}}/sock/docker.sock')"
+docker context use lima
+#--------------------------------------------------------------
+# Docker for Desktop
+#--------------------------------------------------------------
+# brew install docker --cask
 
 # install twingate from appstore
 # install sourcetree from web
